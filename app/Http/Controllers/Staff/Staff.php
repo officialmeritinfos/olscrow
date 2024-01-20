1c41fa8826753bb23fd557aa9fccdbd62f3b4d90
Kopium-Net <87044411+officialmeritinfos@users.noreply.github.com>
2024-01-20 06:40:12 +0100

    Commenced Staff Unit in Admin Section

---
+++ app/Http/Controllers/Staff/Staff.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Staff;
+
+use App\Http\Controllers\BaseController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Staff extends BaseController
+{
+    //landing page
+    public function landingPage()
+    {
+
+    }
+    //new staff
+    public function createStaff()
+    {
+
+    }
+    //process Staff Creation
+    public function processStaffAddition(Request $request)
+    {
+
+    }
+    //edit staff
+    public function editStaff($id)
+    {
+
+    }
+    //process staff Update
+    public function processStaffUpdate(Request $request)
+    {
+
+    }
+}
